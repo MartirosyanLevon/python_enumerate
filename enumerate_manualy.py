4eb7a981bf302b69d66debe4dd6f87599740696c
Levon Martirosyan <levon.martirosyan@dataart.com>
2022-07-22 13:03:55 +0400

    refactoring code

--- enumerate_manualy.py
+++ enumerate_manualy.py
@@ -1,9 +1,9 @@
 def enum(arr):
     for i in range(len(arr)):
-        yield (i,arr[i])
+        yield (i, arr[i])
 
 
 if __name__ == '__main__':
-    a = [1,2,3,4,5,6,7,8,9]
+    a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     for i, e in enum(a):
         print(i, e)
